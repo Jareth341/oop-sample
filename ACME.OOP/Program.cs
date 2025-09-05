b37f4fda8c7c77318a2c8dd09a7688d845d37d28
Jareth Vidal <u202316878@upc.edu.pe>
2025-09-05 17:55:24 -0500

    feat(purchase-order): implement sample usage for PurchaseOrder and Supplier with total calculation

--- ACME.OOP/Program.cs
+++ ACME.OOP/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ACME.OOP.Procurement.Domain.Model.Aggregates;
+using ACME.OOP.Procurement.Domain.Model.ValueObjects;
+using ACME.OOP.SCM.Domain.Model.Aggregates;
+using ACME.OOP.SCM.Domain.Model.ValueObjects;
+using ACME.OOP.Shared.Domain.Model.ValueObjects;
 
-Console.WriteLine("Hello, World!");
+var supplierAddress = new Address
+(
+    street: "123 Main St",
+    number: "1213",
+    city: "Metropolis",
+    stateOrRegion: "NY",
+    postalCode: "12345",
+    country: "USA"
+);
+var supplier =new Supplier("SUP123", "Acme Supplies", supplierAddress);
+
+var purchaseOrder = new PurchaseOrder("PO456", new SupplierId(supplier.Identifier), DateTime.UtcNow, "USD");
+
+purchaseOrder.AddItem(ProductId.New(),10, 15.50m);
+purchaseOrder.AddItem(ProductId.New(),5, 25.00m);
+
+Console.WriteLine($"order total: {purchaseOrder.CalculateOrderTotal()}");
