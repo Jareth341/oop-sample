f13abc0ef08169e2639c556cf554fe92d53072b6
Jareth Vidal <u202316878@upc.edu.pe>
2025-09-05 16:43:25 -0500

    feat(product-id): enhance ProductId value object with documentation and ToString method

--- ACME.OOP/Procurement/Domain/Model/ValueObjects/ProductId.cs
+++ ACME.OOP/Procurement/Domain/Model/ValueObjects/ProductId.cs
@@ -1,8 +1,17 @@
 namespace ACME.OOP.Procurement.Domain.Model.ValueObjects;
 
+/// <summary>
+/// Value Object representing a Product Identifier in the Procurement bounded context.
+/// </summary>
 public record ProductId
 {
     public Guid Id { get; init; }
+    
+    /// <summary>
+    /// Creates a new instance of the <see cref="ProductId"/>  .
+    /// </summary>
+    /// <param name="id">The </param>
+    /// <exception cref="ArgumentException"></exception>
 
     public ProductId(Guid id)
     {
@@ -10,6 +19,14 @@ public record ProductId
             throw new ArgumentException("Product id cannot be empty", nameof(id));
         Id = id;
     }
-    
+    /// <summary>
+    /// Generates a new unique ProductId.
+    /// </summary>
+    /// <returns>A new instance </returns>
     public static ProductId New() => new (Guid.NewGuid());
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => Id.ToString();
 }
